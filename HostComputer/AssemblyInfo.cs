922badb12ed5f926e057910a05ab3b1b4fa54424
长矛CM <1432380670@qq.com>
2025-12-25 10:13:36 +0800

    tttttt

--- HostComputer/AssemblyInfo.cs
+++ HostComputer/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 using System.Windows;
-using System.Reflection;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
--- HostComputer/ViewModels/Overview/SoftwareVersionViewModel.cs
+++ HostComputer/ViewModels/Overview/SoftwareVersionViewModel.cs
@@ -33,14 +33,13 @@ namespace HostComputer.ViewModels.Overview
             SoftwareName = assemblyName.Name ?? "Unknown";
             SoftwareVersion = assembly.GetName().Version?.ToString() ?? "N/A";
             PlcFirmwareVersion = "Melsoft FX5U Ver 1.10"; // 假设从某个配置或服务获取PLC固件版本
-            var gitRevision = assembly.GetCustomAttributes<AssemblyMetadataAttribute>()
-                     .FirstOrDefault(a => a.Key == "GitRevision")?.Value ?? "N/A";
+            GitRevision = assembly.GetCustomAttributes<AssemblyMetadataAttribute>()
+                                  .FirstOrDefault(a => a.Key == "GitRevision")?.Value ?? "N/A";
+            App.Logger.Info("GitRevision: " + GitRevision);
 
-            var commitMessage = assembly.GetCustomAttributes<AssemblyMetadataAttribute>()
-                                .FirstOrDefault(a => a.Key == "GitCommitMessage")?.Value ?? "N/A";
-
-            App.Logger.Info($"GitRevision: {gitRevision}");
-            App.Logger.Info($"CommitMessage: {commitMessage}");
+            CommitMessage = assembly.GetCustomAttributes<AssemblyMetadataAttribute>()
+                                  .FirstOrDefault(a => a.Key == "GitCommitMessage")?.Value ?? "N/A";
+            App.Logger.Info("CommitMessage: " + CommitMessage);
             BuildTime = File.GetLastWriteTime(assembly.Location)
                             .ToString("yyyy-MM-dd HH:mm:ss");
         }
