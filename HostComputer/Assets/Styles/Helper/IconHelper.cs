8b508acdb449778474507ae650b9227d330682df
长矛CM <1432380670@qq.com>
2025-12-29 14:28:45 +0800

    二进制乱码修改

---
+++ HostComputer/Assets/Styles/Helper/IconHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace HostComputer.Assets.Styles.Helper
+{
+    public static class IconHelper
+    {
+        public static readonly DependencyProperty IconFontSizeProperty =
+            DependencyProperty.RegisterAttached(
+                "IconFontSize",
+                typeof(double),
+                typeof(IconHelper),
+                new FrameworkPropertyMetadata(36d, FrameworkPropertyMetadataOptions.Inherits));
+
+        public static void SetIconFontSize(DependencyObject obj, double value)
+            => obj.SetValue(IconFontSizeProperty, value);
+
+        public static double GetIconFontSize(DependencyObject obj)
+            => (double)obj.GetValue(IconFontSizeProperty);
+    }
+}
